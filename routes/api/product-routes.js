094921cc41f56921719eb05437873f84f197615b
D Kornbluth <dwight.kornbluth@gmail.com>
2021-11-05 14:55:05 -0400

    updated tag-routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -53,7 +53,7 @@ router.post('/', (req, res) => {
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
+      if (req.body.tagIds && req.body.tagIDs.length) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
           return {
             product_id: product.id,
@@ -116,6 +116,13 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((products) => res.status(200).json(products))
+    .catch((err) => res.status(400).json(err));
 });
 
 module.exports = router;
